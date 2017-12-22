7e08b0a30bf9d9cdfb0f66add772d70bf5095cd2
WizardZombie2002 <34771870+WizardZombie2002@users.noreply.github.com>
2017-12-22 16:13:58 +0000

    Delete Infinte Geometric Series.py

--- Infinte Geometric Series.py
+++
@@ -1,6 +0,0 @@
-# Sum of an infinite geometric series
-# A painfully simple program by Frogs123
-# We did this in Algebra B last night and I have no other ideas for math programs
-a = float(input("First term in the series: "))
-r = float(input("Common ratio of the series: "))
-print(a/(1-r))
