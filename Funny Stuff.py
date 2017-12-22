f558b77677d5d0987074ed4845f508fbe975e7b9
WizardZombie2002 <34771870+WizardZombie2002@users.noreply.github.com>
2017-12-22 16:13:18 +0000

    Delete Funny Stuff.py

--- Funny Stuff.py
+++
@@ -1,7 +0,0 @@
-hourspernight = input('How many hours do you sleep per night?')
-hoursperweek = float(hourspernight) * 7
-print('You sleep ',hoursperweek,' hours per week.')
-hourspermonth = hoursperweek * 4.35
-print('You sleep ',hourspermonth,' hours a month.')
-hoursperyear = hourspermonth * 12
-print('You sleep ',hoursperyear,' hours a year.')
